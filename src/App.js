a57637e466acbd84c3b5ad2838008ec104848b1c
hudaman0621 <mrhu828@gmail.com>
2020-06-16 13:32:41 -0400

    wat a router?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Link, 
+  Switch 
+} from 'react-router-dom';
 
-function App() {
+// const Home = () => (<h2>Home</h2>)
+
+export default function RouterExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home </Link>
+          </li>
+          <li>
+            <Link to="/Issue">Issue</Link>
+          </li>
+          <li>
+            <Link to="/MoreIssue">More Issue</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+            </Route>
+            <Route path="/Issue">
+              <Issue />
+            </Route>
+
+            <Route path="/MoreIssue">
+              <MoreIssue />
+            </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (
+      <div>
+        <h2>Home</h2>
+      </div>
+  );
+}
+function Issue() {
+  return (
+      <div>
+        <h2>Issue</h2>
+      </div>
+  );
+}
+function MoreIssue() {
+  return (
+      <div>
+        <h2>More Issue......</h2>
+      </div>
+  );
+}
+
